661d9daca293cecf8710a9bfa32831a7b5373721
murat-ozen <121829151+murat-ozen@users.noreply.github.com>
2024-12-30 20:47:35 +0300

    services - interfaces and classes

---
+++ Services/ServiceManager.cs
@@ -0,0 +1,24 @@
+using Services.Contracts;
+
+namespace Services
+{
+    public class ServiceManager : IServiceManager
+    {
+        public readonly IProductService _productService;
+        public readonly ICategoryService _categoryService;
+        public readonly IOrderService _orderService;
+
+        public ServiceManager(IProductService productService, ICategoryService categoryService, IOrderService orderService)
+        {
+            _productService = productService;
+            _categoryService = categoryService;
+            _orderService = orderService;
+        }
+
+        public IProductService ProductService => _productService;
+
+        public ICategoryService CategoryService => _categoryService;
+
+        public IOrderService OrderService => _orderService;
+    }
+}
