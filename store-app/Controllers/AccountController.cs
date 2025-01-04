a93fe226ab67175095c2cca71d06ee956a9de0e9
murat-ozen <121829151+murat-ozen@users.noreply.github.com>
2025-01-04 18:04:50 +0300

    accountcontroller for login, loginmodel and login view

---
+++ store-app/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using store_app.Models;
+
+namespace store_app.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Login([FromForm] LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await _userManager.FindByNameAsync(model.Name);
+                if (user is not null)
+                {
+                    await _signInManager.SignOutAsync();
+                    if ((await _signInManager.PasswordSignInAsync(user, model.Password, false, false)).Succeeded)
+                    {
+                        return Redirect(model?.ReturnUrl ?? "/");
+                    }
+                }
+                ModelState.AddModelError("Error", "Invalid username or password.");
+            }
+            return View();
+        }
+    }
+}
